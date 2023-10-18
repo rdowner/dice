197bd71da0b6526c60a1ca72d3c8c9cf5ff3cf58
Matthew Dillon <dillon@apollo.backplane.com>
2023-10-17 18:25:22 -0700

    DICE - Update rev to 1.15

--- src/dcc/DCC_rev.h
+++ src/dcc/DCC_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
 #define REVISION	14
 #define DATE    "28.12.2015"
-#define VERS    "DCC 1.14"
-#define VSTRING "DCC 1.14 (28.12.2015)"
-#define VERSTAG "\0$VER: DCC 1.14 (28.12.2015)"
+#define VERS    "DCC 1.15"
+#define VSTRING "DCC 1.15 (17.10.2023)"
+#define VERSTAG "\0$VER: DCC 1.15 (17.10.2023)"
